b1384b2ee48fe0f78d64de51b228a5d3ce078b2e
Yusuke Sangenya <longinus.eva@gmail.com>
2021-12-31 23:18:22 +0900

    post

---
+++ example/post.rb
@@ -0,0 +1,19 @@
+require 'co2mini'
+require 'faraday'
+require 'json'
+
+dev = CO2mini.new
+
+conn = Faraday.new(
+  url: 'https://genya0407:31WHIHvMPFUS@datastore.genya0407.net',
+  headers: {'Content-Type' => 'application/json'}
+)
+
+dev.on(:co2) do |op, val|
+  puts "Co2: #{val}ppm"
+  conn.post('/topics/post/co2') do |req|
+    req.body = JSON.generate(created_at: Time.now.to_i, value: val)
+  end.tap { |r| p r.status }
+end
+
+dev.loop
--- lib/co2mini.rb
+++ lib/co2mini.rb
@@ -2,65 +2,37 @@ require "co2mini/version"
 require 'hid_api'
 
 class CO2mini
-  private
-  def _decrypt(key, data)
-    offset  = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]  #"Htemp99e"
-    shuffle = [2, 4, 0, 7, 1, 6, 5, 3];
-
-    phase1 = shuffle.map{|i| data[i] }
-    phase2 = (0..7).map{|i| phase1[i] ^ key[i] }
-    phase3 = (0..7).map{|i| ( (phase2[i] >> 3) | (phase2[ (i-1+8)%8 ] << 5) ) & 0xff }
-    ctmp   = (0..7).map{|i| ( (offset[i] >> 4) | offset[i] << 4 )  & 0xff }
-    result = (0..7).map{|i| (0x100 + phase3[i] - ctmp[i]) & 0xff }
-    return result;
-  end
-
-  def _hex(arr)
-    arr.map{|e| e.to_s(16)}
-  end
-
-  public
-  @@events = {
-    :co2  => 0x50,
-    :temp => 0x42,
+  EVENT = {
+    0x50 => :co2,
+    0x42 => :temp,
   }
 
   def initialize(key = [0x86, 0x41, 0xc9, 0xa8, 0x7f, 0x41, 0x3c, 0xac])
-    @key      = key
     @handlers = {}
+    @device = HidApi.open(0x4d9, 0xa052).tap { |dev| dev.send_feature_report([0x00] + key) }
   end
 
   def on(event, &block)
-    if @@events[event].nil?
-      abort "Invalid event: #{event}"
-    end
     @handlers[event] = block
   end
 
   def loop
-    @device = HidApi.open(0x4d9, 0xa052)
-    ObjectSpace.define_finalizer(self) { @device.close }
-
-    @device.send_feature_report([0x00] + @key)
     while true do
       buf = @device.read(8)
-      res = buf.get_array_of_uint8(0, 8)
-      decrypted = _decrypt(@key, res)
+      data = buf.get_array_of_uint8(0, 8)
 
-      if decrypted[4] != 0x0d or (decrypted[0..2].inject(:+) & 0xff) != decrypted[3]
-        warn "Checksum error #{_hex(data).inspect} => #{_hex(decrypted).inspect}"
-      else
-        op = decrypted[0]
-        val = decrypted[1] << 8 | decrypted[2]
+      event = EVENT[data[0]]
+      next unless @handlers[event]
 
-        case op
-        when @@events[:co2] then
-          @handlers[:co2].call(:co2, val)
-
-        when @@events[:temp] then
-          @handlers[:temp].call(:temp, val / 16.0 - 273.15)
-        end
+      raw_val = data[1] << 8 | data[2]
+      val = case event
+      when :co2
+        raw_val
+      when :temp
+        (raw_val / 16.0 - 273.15).round(1)
       end
+
+      @handlers[event].call(event, val)
     end
   end
 end
