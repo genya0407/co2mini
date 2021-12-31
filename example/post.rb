6b174aff8147659256cde622474406f01a5778f7
Yusuke Sangenya <longinus.eva@gmail.com>
2022-01-01 01:00:20 +0900

    fixpath

--- example/post.rb
+++ example/post.rb
@@ -11,14 +11,14 @@ conn = Faraday.new(
 
 dev.on(:co2) do |op, val|
   puts "Co2: #{val}ppm"
-  conn.post('/topics/post/co2') do |req|
+  conn.post('/topics/co2') do |req|
     req.body = JSON.generate(created_at: Time.now.to_i, value: val)
   end.tap { |r| p r.status }
 end
 
 dev.on(:temp) do |op, val|
   puts "Temp: #{val}"
-  conn.post('/topics/post/temp') do |req|
+  conn.post('/topics/temp') do |req|
     req.body = JSON.generate(created_at: Time.now.to_i, value: val)
   end.tap { |r| p r.status }
 end
